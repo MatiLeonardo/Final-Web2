47e5b4a12895e215bfe7ffb784412ba7d93ccb24
Matias Leonardo Gomez <mati.leonardo@outlook.com>
2025-07-30 16:45:12 -0300

    Inicio

---
+++ router.php
@@ -0,0 +1,21 @@
+<?php
+require_once '';
+
+define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
+
+$action = 'home';
+
+if (!empty($_GET['action'])) {
+    $action = $_GET['action'];
+}
+
+$params = explode('/', $action);
+
+switch ($params[0]) {
+    case 'home':
+        showHome();
+        break;
+    default:
+        echo ('404 Page not found');
+        break;
+}
